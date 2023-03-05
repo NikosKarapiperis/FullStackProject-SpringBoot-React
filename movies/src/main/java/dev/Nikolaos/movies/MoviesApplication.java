52baa6922000e017cc9bf16ff5b16e544cb18d61
Νικόλαος-Ιωάννης Καραπιπερης <92748811+NikosKarapiperis@users.noreply.github.com>
2023-03-06 01:20:34 +0200

    Update MoviesApplication.java

--- movies/src/main/java/dev/Nikolaos/movies/MoviesApplication.java
+++ movies/src/main/java/dev/Nikolaos/movies/MoviesApplication.java
@@ -9,5 +9,19 @@ public class MoviesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 
 }
